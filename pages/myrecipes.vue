bf70b589cd8e2c4f4577b9fdbff9b1d6be88a4a7
Liana <AbramyanLV20@st.ithub.ru>
2023-03-23 12:47:06 +0300

    myrecipes page style

--- pages/myrecipes.vue
+++ pages/myrecipes.vue
@@ -1,19 +1,90 @@
 <template>
   <div class="container">
     <h1>Ваши рецепты</h1>
-    <h3>Пока рецептов нет</h3>
+<!--    <h3>Пока рецептов нет</h3>-->
+    <button class="addRecipeBtn" @click="openMenuAddRecipe">Добавить рецепт</button>
+    <div class="addRecipeForm" :style="{display: openMenu ? 'flex': 'none'}">
+      <form @submit.prevent="addRecipes">
+        <input type="text" name="title" placeholder="Название">
+        <input type="text" name="description" placeholder="Описание">
+        <input type="text" name="img" placeholder="Ссылка на изображение">
+        <textarea class="inp-long" name="ingredients" placeholder="Ингредиенты"></textarea>
+        <textarea class="inp-long" name="steps" placeholder="Способ приготовления"></textarea>
+        <button type="submit" class="addRecipeBtn">Добавить</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name:"my-recipes-page"
+  name:"my-recipes-page",
+  data(){
+    return {
+      openMenu: false,
+      recipeTitle: "",
+      recipeDesc: "",
+      recipeImg: "",
+      recipeIng: [],
+      recipeSteps: []
+    }
+  },
+  methods:{
+    openMenuAddRecipe(){
+      this.openMenu = !this.openMenu
+    },
+    addRecipes: async function(e){
+
+    }
+  }
 }
 </script>
 
 <style scoped>
+.container{
+  text-align: center;
+  position: relative;
+}
 .container h3{
   text-align: center;
   font-size: 35px;
 }
+.container .addRecipeBtn{
+  padding: 10px;
+  font-size: 30px;
+  cursor: pointer;
+  background-color: #729343;
+  color: white;
+  width: 400px;
+  margin-bottom: 50px;
+  border-radius: 10px;
+  border: none;
+}
+.addRecipeForm{
+  display: flex;
+  position: absolute;
+  left: 41%;
+  justify-content: center;
+}
+.addRecipeForm form{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+}
+.inp-long{
+  width: 400px;
+  font-size: 25px;
+  align-items: flex-end;
+  height: 200px;
+
+}
+input{
+  width: 400px;
+  height: 50px;
+  border-radius: 5px;
+  font-size: 25px;
+}
+
 </style>
