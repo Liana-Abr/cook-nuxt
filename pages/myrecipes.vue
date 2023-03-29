709fb11e419d017ec71ba214af8cd89d7e719ea6
Liana <AbramyanLV20@st.ithub.ru>
2023-03-29 16:06:29 +0300

    ADD recipes in database

--- pages/myrecipes.vue
+++ pages/myrecipes.vue
@@ -17,15 +17,27 @@
 <!--        protein-->
 <!--        carb-->
 <!--        calories-->
-        <input type="text" name="name" placeholder="Название">
-        <input type="text" name="description" placeholder="Описание">
-        <input type="text" name="category" placeholder="Категория(Например: Завтрак)">
-        <input type="text" name="img" placeholder="Ссылка на изображение">
-        <textarea class="inp-long" name="ingredients" placeholder="Ингредиенты"></textarea>
-        <textarea class="inp-long" name="steps" placeholder="Способ приготовления"></textarea>
-        <button type="submit" class="addRecipeBtn">Добавить</button>
+        <input type="text" name="name" placeholder="Название" v-model="recipeName">
+        <input type="text" name="description" placeholder="Описание" v-model="recipeDesc">
+        <input type="text" name="category" placeholder="Категория(Например: Завтрак)" v-model="recipeCategory">
+        <input type="text" name="time" placeholder="Время готовки" v-model="recipeTime">
+        <input type="text" name="img" placeholder="Ссылка на изображение" v-model="recipeImg">
+        <input type="text" name="tags" placeholder="Теги" v-model="formattedTags">
+        <input type="text" name="ingredients" placeholder="Ингредиенты" v-model="formattedIng">
+        <input type="text" name="steps" placeholder="Способ приготовления" v-model="formattedSteps">
+<!--        <textarea class="inp-long" name="ingredients" placeholder="Ингредиенты" v-model="recipeIng"></textarea>-->
+<!--        <textarea class="inp-long" name="steps" placeholder="Способ приготовления"  v-model="recipeSteps"></textarea>-->
+        <p>КБЖУ</p>
+        <input type="text" name="fat" placeholder="Жиры" v-model="recipeFat">
+        <input type="text" name="protein" placeholder="Белки" v-model="recipeProt">
+        <input type="text" name="carb" placeholder="Углеводы" v-model="recipeCarb">
+        <input type="text" name="calories" placeholder="Калорийность" v-model="recipeCal">
+        <button type="submit" class="addRecipeBtn" >Добавить</button>
       </form>
+
     </div>
+<!--    <input type="text" v-model="newTag" />-->
+<!--    <button @click="splitArray">Добавить</button>-->
   </div>
 </template>
 
@@ -35,11 +47,45 @@ export default {
   data(){
     return {
       openMenu: false,
-      recipeTitle: "",
+      recipeName: "",
       recipeDesc: "",
+      recipeCategory: "",
+      recipeTime: "",
       recipeImg: "",
+      recipeTags: [],
       recipeIng: [],
-      recipeSteps: []
+      recipeSteps: [],
+      recipeFat: "",
+      recipeProt: "",
+      recipeCarb: "",
+      recipeCal: "",
+      // name, description, category, time, image, tags, ingredients, steps, fat, protein, carb, calories
+    }
+  },
+  computed: {
+    formattedTags :{
+      get(){
+        return this.recipeTags.join(",");
+      },
+      set(value){
+        this.recipeTags = value.split(',');
+      }
+    },
+    formattedIng :{
+      get(){
+        return this.recipeIng.join(",");
+      },
+      set(value){
+        this.recipeIng = value.split(',');
+      }
+    },
+    formattedSteps :{
+      get(){
+        return this.recipeSteps.join(",");
+      },
+      set(value){
+        this.recipeSteps = value.split(',');
+      }
     }
   },
   methods:{
@@ -47,7 +93,30 @@ export default {
       this.openMenu = !this.openMenu
     },
     addRecipes: async function(e){
-
+      e.preventDefault()
+      let body = {
+        name: this.recipeName,
+        description: this.recipeDesc ,
+        category: this.recipeCategory,
+        time:this.recipeTime ,
+        image: this.recipeImg,
+        tags:this.recipeTags ,
+        ingredients:this.recipeIng ,
+        steps:this.recipeSteps ,
+        fat: this.recipeFat,
+        protein: this.recipeProt,
+        carb:this.recipeCarb ,
+        calories:this.recipeCal ,
+      }
+      const data = await fetch("http://localhost:3001/recipes/add",{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(body)
+      }).then(res => res.json());
+      console.log(data)
     }
   }
 }
@@ -79,7 +148,7 @@ export default {
 .addRecipeForm{
   display: flex;
   position: absolute;
-  left: 41%;
+  left: 45%;
   justify-content: center;
 }
 .addRecipeForm form{
@@ -88,6 +157,9 @@ export default {
   align-items: center;
   flex-direction: column;
   gap: 20px;
+}
+.addRecipeForm p{
+  font-size: 30px;
 }
 .inp-long{
   width: 400px;
